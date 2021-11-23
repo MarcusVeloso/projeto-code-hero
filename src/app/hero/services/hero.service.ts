1d7e1c8dfb0d2fa6c9079fdb974bb0ad3709d3e7
Marcus Veloso <desenvolvedoroficial@gmail.com>
2021-11-23 02:20:58 -0300

    infra básica para consultas a api

---
+++ src/app/hero/services/hero.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError } from "rxjs/operators";
+import { BaseService } from 'src/app/services/base.service';
+
+@Injectable( )
+export class HeroService extends BaseService {
+
+  constructor(private http: HttpClient) { super() }
+
+  obterTodos(): Observable<any[]>{
+    return this.http
+               .get<any[]>(this.UrlSreviceV1 + "characters" + super.obterAutenticacao(), super.obterHeaderJson())
+               .pipe(catchError(super.serviceError));
+  }
+
+  obterPorId(id:any): Observable<any>{
+    return this.http
+               .get<any>(this.UrlSreviceV1 + "characters/" + id + super.obterAutenticacao(), super.obterHeaderJson())
+               .pipe(catchError(super.serviceError));
+  }
+}
---
+++ src/app/services/base.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpHeaders } from "@angular/common/http";
+import { throwError } from 'rxjs';
+
+@Injectable()
+export abstract class BaseService {
+
+  protected UrlSreviceV1: string = environment.urlV1;
+
+  protected obterHeaderJson() {
+    return {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    };
+}
+
+  protected extractData(response: any){
+    return response.data || {};
+  }
+
+  protected serviceError(response: Response | any){
+
+    return throwError(response);
+  }
+
+  protected obterAutenticacao(){
+    return `?ts=1&apikey=${environment.apikey}&hash=${environment.hash}`;
+  }
+}
