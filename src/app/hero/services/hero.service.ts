543b1a52b4969deb00901d53aed9bc6ef835cfe4
Marcus Veloso <desenvolvedoroficial@gmail.com>
2021-11-24 16:48:27 -0300

    ajustar layout responsivo lista hero

--- src/app/hero/services/hero.service.ts
+++ src/app/hero/services/hero.service.ts
@@ -11,8 +11,7 @@ export class HeroService extends BaseService {
 
   obterTodos(): Observable<any[]>{
     return this.http
-               .get<any[]>(this.UrlSreviceV1)
-              //  .get<any[]>(this.UrlSreviceV1 + "characters" + super.obterAutenticacao(), super.obterHeaderJson())
+               .get<any[]>(this.UrlSreviceV1 + "characters" + super.obterAutenticacao(), super.obterHeaderJson())
                .pipe(catchError(super.serviceError));
   }
 
